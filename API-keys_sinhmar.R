068c8f629ad54849950c1a331e917bad7f882e9e
arun sinhmar <37594480+arunsinhmar@users.noreply.github.com>
2019-05-10 22:03:03 -0400

    Add files via upload

---
+++ API-keys_sinhmar.R
@@ -0,0 +1,11 @@
+# enter the consumer key provided by Twitter application
+api_key = "aJcGGZaAOoXUhh258HSSvNgrK" 
+
+# enter the consumer secret key supplied by Twitter
+api_secret = "pMhBHaTBNAodKrlKSnpAsL1IM1aAWJDlLpFdmuWolFGINNChiU" 
+
+# enter the access token
+access_token = "163399299-lAWo2kqeX0sDRpPf8KdAhCrA00C7tKWTbeJgc2JZ" 
+
+# enter the access secret supplied by Twitter
+access_token_secret =  "NqEFxybTPHoBSAGZKi3QAB98nfRokzE3YbApkQTMuc5AU"
